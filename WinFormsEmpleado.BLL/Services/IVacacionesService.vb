d26ea74046f7a23656709750ebb30a3d7cd8068d
gbreck-ni <152242617+gbreck-ni@users.noreply.github.com>
2024-01-14 13:42:38 -0600

    Agregado: Implementacion de interfaz CRUD  en clase vacaciones service

---
+++ WinFormsEmpleado.BLL/Services/IVacacionesService.vb
@@ -0,0 +1,7 @@
+﻿Imports WinFormsEmpleado.Models
+
+Public Interface IVacacionesService
+    Function Insert(entity As Vacaciones) As Boolean
+    Function Delete(id As Int32) As Boolean
+    Function GetAll(empleadoId As Int32) As List(Of Vacaciones)
+End Interface
---
+++ WinFormsEmpleado.BLL/Services/VacacionesService.vb
@@ -0,0 +1,22 @@
+﻿Imports WinFormsEmpleado.DAL
+Imports WinFormsEmpleado.Models
+
+Public Class VacacionesService
+    Implements IVacacionesService
+    Private ReadOnly _repository As IVacacionesRepository
+    Public Sub New(repository As IVacacionesRepository)
+        _repository = repository
+    End Sub
+
+    Public Function Insert(entity As Vacaciones) As Boolean Implements IVacacionesService.Insert
+        Return _repository.Insert(entity)
+    End Function
+
+    Public Function Delete(id As Integer) As Boolean Implements IVacacionesService.Delete
+        Return _repository.Delete(id)
+    End Function
+
+    Public Function GetAll(empleadoId As Integer) As List(Of Vacaciones) Implements IVacacionesService.GetAll
+        Return _repository.GetAll(empleadoId)
+    End Function
+End Class
