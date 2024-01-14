1959a73ca448dbb72b9d6e7e17a1a7f45745bc55
gbreck-ni <152242617+gbreck-ni@users.noreply.github.com>
2024-01-14 13:39:46 -0600

    Agregado: Interfaz y clase empleado service en capa de negocio

---
+++ WinFormsEmpleado.BLL/Services/EmpleadoService.vb
@@ -0,0 +1,27 @@
+﻿Imports WinFormsEmpleado.DAL
+Imports WinFormsEmpleado.Models
+
+Public Class EmpleadoService
+    Implements IEmpleadoService
+    Private ReadOnly _repository As IEmpleadoRepository
+
+    Public Sub New(repository As IEmpleadoRepository)
+        _repository = repository
+    End Sub
+
+    Public Function Insert(entity As Empleado) As Boolean Implements IEmpleadoService.Insert
+        Return _repository.Insert(entity)
+    End Function
+
+    Public Function Delete(id As Integer) As Boolean Implements IEmpleadoService.Delete
+        Return _repository.Delete(id)
+    End Function
+
+    Public Function Update(entity As Empleado) As Boolean Implements IEmpleadoService.Update
+        Return _repository.Update(entity)
+    End Function
+
+    Public Function GetAll() As List(Of Empleado) Implements IEmpleadoService.GetAll
+        Return _repository.GetAll()
+    End Function
+End Class
---
+++ WinFormsEmpleado.BLL/Services/IEmpleadoService.vb
@@ -0,0 +1,8 @@
+﻿Imports WinFormsEmpleado.Models
+
+Public Interface IEmpleadoService
+    Function Insert(entity As Empleado) As Boolean
+    Function Delete(id As Int32) As Boolean
+    Function Update(entity As Empleado) As Boolean
+    Function GetAll() As List(Of Empleado)
+End Interface
