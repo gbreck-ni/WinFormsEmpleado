36978adc2e629b2cf4967ad851606c5fb6b3aa84
gbreck-ni <152242617+gbreck-ni@users.noreply.github.com>
2024-01-14 14:14:30 -0600

    Presentacion: agregado, clase program, que permite configurar la injección de dependencias para acceso a las interfaces en los formularios requeridos.

--- WinFormsEmpleado/FormPanel.vb
+++ WinFormsEmpleado/FormPanel.vb
@@ -2,4 +2,12 @@
     Private Sub FormPanel_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub btnFormEmpleado_Click(sender As Object, e As EventArgs) Handles btnFormEmpleado.Click
+
+    End Sub
+
+    Private Sub btnFormVacaciones_Click(sender As Object, e As EventArgs) Handles btnFormVacaciones.Click
+
+    End Sub
 End Class
---
+++ WinFormsEmpleado/Program.vb
@@ -0,0 +1,31 @@
+﻿Imports Microsoft.Extensions.DependencyInjection
+Imports WinFormsEmpleado.BLL
+Imports WinFormsEmpleado.DAL
+
+Module Program
+    Public Sub Main()
+        '
+        ' Dependency Injection
+        '
+        Dim services = New ServiceCollection()
+        SetServiceContainer(services)
+
+        Dim serviceProvider As ServiceProvider = services.BuildServiceProvider()
+        Dim initForm = serviceProvider.GetRequiredService(Of FormPanel)()
+        Application.Run(initForm)
+
+    End Sub
+
+    Private Sub SetServiceContainer(services As ServiceCollection)
+        services.AddScoped(Of FormPanel)()
+        services.AddScoped(Of FormEmpleado)()
+
+        services.AddSingleton(Of DBTestContext)()
+
+        services.AddScoped(Of IEmpleadoService, EmpleadoService)()
+        services.AddScoped(Of IEmpleadoRepository, EmpleadoRepository)()
+
+        services.AddScoped(Of IVacacionesService, VacacionesService)()
+        services.AddScoped(Of IVacacionesRepository, VacacionesRepository)()
+    End Sub
+End Module
