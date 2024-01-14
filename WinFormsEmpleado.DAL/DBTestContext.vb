2091fdddb11e38c5d02b55214c9eea16136cd5d1
gbreck-ni <152242617+gbreck-ni@users.noreply.github.com>
2024-01-14 13:00:24 -0600

    Agregado: Capa de acceso a datos y DBContext

---
+++ WinFormsEmpleado.DAL/DBTestContext.vb
@@ -0,0 +1,7 @@
+﻿Imports System.Data.SqlClient
+
+Public Class DBTestContext
+    Protected Function DbConnection() As SqlConnection
+        Return New SqlConnection("Server = localhost\SQLEXPRESS02; Database = Test; Trusted_Connection = True; TrustServerCertificate = true")
+    End Function
+End Class
