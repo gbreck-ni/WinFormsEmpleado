eb62419d735be33fb4bc8f990ac48c6b6033ac79
gbreck-ni <152242617+gbreck-ni@users.noreply.github.com>
2024-01-14 14:38:15 -0600

    Presentacion: agregado, llamadas a formulario de administración de vacaciones desde el panel principal

--- WinFormsEmpleado/FormPanel.vb
+++ WinFormsEmpleado/FormPanel.vb
@@ -1,9 +1,12 @@
 ﻿Public Class FormPanel
     Private ReadOnly _frmEmpleado As FormEmpleado
-    Public Sub New(frmEmpleado As FormEmpleado)
+    Private ReadOnly _frmVacaciones As FormVacaciones
+    Public Sub New(frmEmpleado As FormEmpleado,
+                   frmVacaciones As FormVacaciones)
         ' Esta llamada es exigida por el diseñador.
         InitializeComponent()
         _frmEmpleado = frmEmpleado
+        _frmVacaciones = frmVacaciones
     End Sub
 
     Private Sub btnFormEmpleado_Click(sender As Object, e As EventArgs) Handles btnFormEmpleado.Click
@@ -12,5 +15,7 @@
     End Sub
 
     Private Sub btnFormVacaciones_Click(sender As Object, e As EventArgs) Handles btnFormVacaciones.Click
+        Dim form = _frmVacaciones
+        form.ShowDialog(Me)
     End Sub
 End Class
